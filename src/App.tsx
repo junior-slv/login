807d30dabcd75cf665fd4a5f088fe9fe2c66374b
Junior <dirceusilva0422@gmail.com>
2022-11-22 16:22:56 -0300

    added hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import {useEffect} from 'react'
 import './App.css';
+const url = "https://robby-bank.herokuapp.com/users"
+type Response = {
+  email: any;
+};
+const App = () => {
+  const [usuario, setUsuario] = useState()
 
-function App() {
+
+  useEffect(() => {
+    axios
+    .get<Response>(url)
+    .then((res) => {
+      setUsuario(res.data.email);
+    })
+  
+  })
+  
   return (
-    <div className="App">
+    <div className="container">
+      <div className="login-menu">
+        <div className="top-menu">
+          <p>{usuario}</p>
+          <p>{usuario}</p>
+          <input type="email" name="" id="" />
+        </div>
+        <div className="bottom-menu">
+          <p>Senha</p>
+          <input type="password" name="" id="" />
+        </div>
+      </div>
     </div>
   );
 }
